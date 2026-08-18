40252e355684b15513976b3d9d4904f57601eda3
isha-web1 <isha.web1@gmail.com>
2026-08-19 00:57:35 +0600

    write posts,comment schema & setup express server&database

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+
+const app : Application = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+
+export default app;
