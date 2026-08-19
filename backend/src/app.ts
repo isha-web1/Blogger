757b8fef2320437fa4986c3144c154dfc25b81ac
isha-web1 <isha.web1@gmail.com>
2026-08-19 15:00:20 +0600

    add better-auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,15 @@
 import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.router";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app : Application = express();
+
+app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use('/posts', postRouter)
 
 
 app.get("/", (req, res) => {
