dbeed743ee2846921d2a9b018444514807bbb792
isha-web1 <isha.web1@gmail.com>
2026-08-25 15:39:36 +0600

    add globalErrorHandler & notFound router system

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import { commentRouter } from "./modules/comment/comment.router";
+import errorHandler from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
 
 const app : Application = express();
 
@@ -21,9 +23,15 @@ app.use('/posts', postRouter)
 app.use("/comments", commentRouter);
 
 
+
+
+
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use(notFound)
+app.use(errorHandler)
 
 
 export default app;
